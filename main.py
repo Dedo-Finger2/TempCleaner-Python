023ae10825940e8f06ee69c5e28fd7c2a1a6877f
Antonio Mauricio <antonioimportant@gmail.com>
2023-11-05 03:59:07 -0800

    instructions

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+| -------------
+| Temp Cleaner
+| -------------
+| Algoritmo que limpa os diretórios temp e %temp% do Windows.
+"""
+# Importando bibliotecas
+import os
+import shutil
+
+# Pegar o diretório temp
+# Pegar diretório %temp%
+# Listar itens dentro da temp
+# Listar itens dentro da %temp%
+# Para cada item dentro da temp
+# Deletar o item
+
